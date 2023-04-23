69cc401b3ee9e80688ef28a347d6f77f12c01f74
vlad <vlad.bras02@gmail.com>
2023-04-23 10:00:25 +0200

    feat(mysql): connect to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NANE,
+      autoLoadEntities: true,
+      synchronize: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
