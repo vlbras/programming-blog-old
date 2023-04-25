4f1be995915e2d445a958640444b884420551f94
vlad <vlad.bras02@gmail.com>
2023-04-25 13:39:05 +0200

    feat(post): add validation

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,1 +1,15 @@
-export class CreatePostDto {}
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class CreatePostDto {
+    @IsString()
+    @IsNotEmpty()
+    title: string
+
+    @IsString()
+    @IsNotEmpty()
+    content: string
+
+    @IsString()
+    @IsNotEmpty()
+    topic: string
+}
