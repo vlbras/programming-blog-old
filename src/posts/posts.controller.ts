038eca5f2373475a7157e7b1546d2a8389f8dea4
vlad <vlad.bras02@gmail.com>
2023-05-06 08:17:13 +0200

    feat(post): controller optimization

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -4,25 +4,39 @@ import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 import { ApiForbiddenResponse, ApiTags } from '@nestjs/swagger';
 
-@ApiTags('posts')
-@ApiForbiddenResponse({description: 'Forbidden.'})
-@Controller('posts')
+@ApiForbiddenResponse({ description: 'Forbidden.' })
+@Controller('')
 export class PostsController {
-  constructor(private readonly postsService: PostsService) {}
+  constructor(private readonly postsService: PostsService) { }
 
-  @Post()
+  @Post('posts')
+  @ApiTags('posts')
   create(@Body() createPostDto: CreatePostDto) {
     return this.postsService.create(createPostDto);
   }
 
-  @Get()
+  @Get('posts')
+  @ApiTags('posts')
   findAll() {
     return this.postsService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.postsService.findOne(+id);
+  @Get('posts/:id')
+  @ApiTags('posts')
+  findOne(@Param('id') id: number) {
+    return this.postsService.findOne(id);
+  }
+
+  @Patch('posts/:id')
+  @ApiTags('posts')
+  update(@Param('id') id: number, @Body() updatePostDto: UpdatePostDto) {
+    return this.postsService.update(id, updatePostDto);
+  }
+
+  @Delete('posts/:id')
+  @ApiTags('posts')
+  remove(@Param('id') id: number) {
+    return this.postsService.remove(id);
   }
 
   @Patch(':id')
