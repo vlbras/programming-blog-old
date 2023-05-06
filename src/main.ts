a99b2302e158f63960874adf26b1b921e6f6343c
vlad <vlad.bras02@gmail.com>
2023-05-06 08:25:18 +0200

    style: use handlebars for blog pages

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { join } from 'path';
+import * as hbs from 'hbs';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -13,6 +17,11 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
       transform: true,
     }))
+
+  app.useStaticAssets(join(__dirname, '..', 'public'))
+  app.setBaseViewsDir(join(__dirname, '..', 'views'))
+  // hbs.registerPartials(join(__dirname, '..', 'views/partials'))
+  app.setViewEngine('hbs')
 
   const options = new DocumentBuilder()
     .setTitle('Programming Blog')
